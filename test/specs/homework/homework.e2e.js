74e11b9b090bcdf0f3589cc89ecbf99f906b5a42
Monika <monika.jitka@gmail.com>
2023-11-01 20:12:15 +0100

    Registration

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -2,7 +2,37 @@ describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
 
-        // sem vypracuj domácí úkol
+        await browser.reloadSession();
+
+        await browser.url('/registrace');
+
+        await browser.saveScreenshot('registrace.png');})
+    
+    it("registration new user" , async () => {
+       
+        const nameField = await $('#name');
+        const emailField = await $('#email');
+        const passwordField = await $('#password');
+        const userName = "Alois Krupička";
+        const email = "krupicka@seznam.cz";
+        const password = "Krupicka123";
+        const passwordConfirm = await $('#password-confirm');
+        const loginButton = await $('.btn-primary');
+
+
+        await nameField.setValue(userName);
+        
+        await emailField.setValue(email);
+        
+        await passwordField.setValue(password);
+
+        await passwordConfirm.setValue(password);
+        
+        await loginButton.click();
+
+        await browser.pause(5000);
+
+        
 
     });
 
